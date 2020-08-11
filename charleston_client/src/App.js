2b5889003afe0d67177d10abfca227948dcc473b
Will Tidwell <willtidwell@Wills-Air.hsd1.sc.comcast.net>
2020-08-11 12:54:13 -0400

    added axios, react-router, react-router-dom, tested API, added .env

--- charleston_client/src/App.js
+++ charleston_client/src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
 import './App.css';
+import axios from 'axios'
 
 function App() {
+  const app_id = process.env.REACT_APP_app_id
+  const app_key = process.env.REACT_APP_app_key
+
+  useEffect(() => {
+    const makeAPICall = () => {
+      axios
+        .get(`https://api.edamam.com/search?q=chicken&app_id=${app_id}&app_key=${app_key}`)
+        .then((res) => {
+          const data = res.data;
+          console.log(data)
+        })
+        .catch(console.error);
+    };
+    makeAPICall();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <h1>Hello</h1>
+
+  
     </div>
   );
 }
